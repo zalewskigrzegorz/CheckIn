7147726299afa8e5c24161de4b1c47a16ea5e070
Grzegorz Zalewski <maksim009@gmail.com>
2020-05-06 20:16:07 +0200

    Swipe list element

--- components/CheckPoint.js
+++ components/CheckPoint.js
@@ -1,18 +1,12 @@
 import React from 'react'
-import { Avatar, Card, IconButton } from 'react-native-paper'
+import { Avatar, Card, Badge } from 'react-native-paper'
 export default function CheckPoint (prop) {
   return (
     <Card.Title
       title={prop.name}
       left={() => <Avatar.Image size={42} source={prop.image} />}
       right={() => (
-        <IconButton
-          size={24}
-          icon='map-marker-check'
-          onPress={() => {
-            console.log('Check in')
-          }}
-        />
+        <Badge>{Math.floor(Math.random() * (50 + 1))}</Badge>
       )}
     />
   )
--- components/CheckPointList.js
+++ components/CheckPointList.js
@@ -1,8 +1,10 @@
-import { FlatList, RefreshControl, View } from 'react-native'
+import { Text, RefreshControl, View, StyleSheet } from 'react-native'
+import {SwipeableFlatList} from 'react-native-swipeable-flat-list';
 import React from 'react'
 import CheckPoint from '../components/CheckPoint'
 import { useStateValue } from '../context/StateContext'
 import Loading from './Loading'
+import { Colors} from 'react-native-paper'
 
 function CheckPointList (props) {
   // test refresh code
@@ -29,13 +31,19 @@ function CheckPointList (props) {
   } else {
     return (
       <View style={{ width: '100%', height: '100%' }}>
-        <FlatList
+        <SwipeableFlatList
           data={props.data}
-          key
+          key={props.id}
           onRefresh={onRefresh}
           refreshing={isRefreshing}
           renderItem={({ item }) => (
-            <CheckPoint id={item.id} name={item.name} image={item.image} />
+            <CheckPoint id={item.id} name={item.name} style={{height: 60}} image={item.image} />
+          )}
+          renderLeft={({ item }) => (
+            <Text style={styles.left}>Check in</Text>
+          )}
+          renderRight={({ item }) => (
+            <Text style={styles.right}>Check Out</Text>
           )}
           keyExtractor={item => item.id}
           refreshControl={
@@ -51,3 +59,26 @@ function CheckPointList (props) {
   }
 }
 export default CheckPointList
+
+const styles = StyleSheet.create({
+  left: {
+    width: "50%",
+    height: 60,
+    padding: 10,
+    backgroundColor: Colors.green800,
+    color: Colors.white,
+    textAlignVertical: 'center',
+    textAlign:'left',
+    fontWeight: 'bold',
+  },
+  right: {
+    width: "50%",
+    height: 60,
+    padding: 10,
+    backgroundColor: Colors.red800,
+    color: Colors.white,
+    textAlignVertical: 'center',
+    textAlign: 'right',
+    fontWeight: 'bold',
+  }
+})
